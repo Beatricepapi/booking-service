11bec0a21d0df8df87a7a48d5f0d2127e963ef49
Beatrice Papi <G00461762@atu.ie>
2026-04-27 11:26:59 +0100

    Class in  model package

---
+++ src/main/java/atu/ie/bookingservice/model/Booking.java
@@ -0,0 +1,29 @@
+package atu.ie.bookingservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long eventId;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private int quantity;
+
+    private double totalPrice;
+}
