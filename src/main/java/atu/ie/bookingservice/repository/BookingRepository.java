0d70f4f006f397dc026af82f1498a9eb7974c306
Beatrice Papi <G00461762@atu.ie>
2026-04-27 11:40:08 +0100

    Class in repository package

---
+++ src/main/java/atu/ie/bookingservice/repository/BookingRepository.java
@@ -0,0 +1,10 @@
+package atu.ie.bookingservice.repository;
+
+import atu.ie.bookingservice.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByUserId(Long userId);
+}
